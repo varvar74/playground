6871ad0bc151c0966621726b87cf257ad7ac2ed9
varvar74 <anton.filippenko@gmail.com>
2025-11-14 08:51:13 -0500

    Fix filter binding signal

---
+++ client/karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        random: false
+      }
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadless'],
+    singleRun: true
+  });
+};
